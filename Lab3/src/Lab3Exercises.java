ffe892661f92980472dc180ca3cc16b5192a5d7d
victor <victor@10.31.58.168>
2017-11-15 12:12:23 -0400

    JFrame Serttings
    Changes the window title to the programmer's name, sets the size of the
    screen to 200x200 and sets its location to the center of the screen.

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -2,7 +2,7 @@ import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Victor A. Nazario Morales");
+		JFrame myFrame = new JFrame("Victor A. Nazario Morales");  //Sets the title of the screen
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		myFrame.setLocation(600, 300);
 		myFrame.setSize(200, 200);
